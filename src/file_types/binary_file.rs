36edc4fc20bf519025e4716580accc318ed5d895
Luna Kilborn <leland.kilborn@hotmail.com>
2022-04-25 16:11:24 -0400

    Implement single bit reading in binary_file
    I changed the BufReader to an iterator because it's a lot more efficient and useful for what it's doing. Armed with that upgrade, I proceeded to implement the ability to read a single bit as a bool, and to buffer another byte, which happens automatically, but can be done manually if needed. As usual, I also added in a new test to ensure that it's working. the test just opens a sample file and ensures that it gets the first character and some of the second right.

--- src/file_types/binary_file.rs
+++ src/file_types/binary_file.rs
@@ -1,17 +1,41 @@
 use std::fs::File;
-use std::io::BufReader;
+use std::io::Read;
+use std::io::Bytes;
 use std::collections::VecDeque;
 
 pub struct BinaryFile {
-    internal_file: BufReader<File>,
+    file_iterator: Bytes<File>,
     buffer: VecDeque<bool>,
 }
 
 impl BinaryFile {
     pub fn open_file(path: &str) -> std::io::Result<BinaryFile> {
         Ok(BinaryFile {
-            internal_file: BufReader::new(File::open(path)?),
+            file_iterator: File::open(path)?.bytes(),
             buffer: VecDeque::new(),
         })
+    }
+
+    pub fn load_byte(&mut self) -> Option<u8> {
+        let next_byte = match self.file_iterator.next() {
+            Some(byte) => byte.unwrap(),
+            None => return None,
+        };
+        for i in (0..8).rev() {
+            self.buffer.push_back((next_byte & (1 << i)) >= 1);
+        };
+        Some(next_byte)
+    }
+
+    pub fn read_bit(&mut self) -> Option<bool> {
+        match self.buffer.pop_front() {
+            Some(bit) => Some(bit),
+            None => {
+                match self.load_byte() {
+                    Some(_) => self.read_bit(),
+                    None => None,
+                }
+            },
+        }
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -13,4 +13,22 @@ mod tests {
         let inv_bin_f = BinaryFile::open_file("sample_files\\invalid_path");
         assert!(inv_bin_f.is_err());
     }
+
+    #[test]
+    fn binary_file_reading() {
+        let bin_f_r = BinaryFile::open_file("sample_files\\valid_path");
+        assert!(bin_f_r.is_ok());
+        let mut bin_f = bin_f_r.unwrap();
+        assert!(!bin_f.read_bit().unwrap());
+        assert!(bin_f.read_bit().unwrap());
+        assert!(bin_f.read_bit().unwrap());
+        assert!(bin_f.read_bit().unwrap());
+        assert!(!bin_f.read_bit().unwrap());
+        assert!(bin_f.read_bit().unwrap());
+        assert!(!bin_f.read_bit().unwrap());
+        assert!(!bin_f.read_bit().unwrap());
+        assert!(!bin_f.read_bit().unwrap());
+        assert!(bin_f.read_bit().unwrap());
+        assert!(bin_f.read_bit().unwrap());
+    }
 }
